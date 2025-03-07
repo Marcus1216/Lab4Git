8f90a91e35576c68885fd86d91026456eab36cbc
Marcus <mwongmanhei@gmail.com>
2025-03-07 14:11:18 +0800

    V2

--- src/Mainnn.java
+++ src/Mainnn.java
@@ -1,6 +1,7 @@
 public class Mainnn {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("ERM i dont know really I just wanna push");
     }
     //Branch 2
 
